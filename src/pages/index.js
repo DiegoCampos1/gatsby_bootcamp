e512939c412e68373b64626a50fe83f2674e5fa2
diego <diegodiko05@gmail.com>
2020-10-09 11:33:41 -0300

    My first layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,15 @@
 import React from "react"
 import { Link } from 'gatsby'
-import Footer from "../components/footer"
-import Header from "../components/header"
-
+import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <div>
-      <Header />
+      <Layout>
       <h1>Hello</h1>
       <h2>My name is Diego Campos, a front-end developer</h2>
       <p>Need a developer? <a href="/contact" rel="noopener noreferrer">Contact me</a></p> {/* Melhor usar para links externos */} 
       <p>Need a developer? <Link to="/contact">Contact me</Link></p> {/* Melhor usar para links interno */}
-      <Footer />
-    </div>
+      </Layout>
   )
 }
 
