7ecda2b4db422b5e512b7983f48cb35c45259fd0
diego <diegodiko05@gmail.com>
2020-10-11 12:58:13 -0300

    Criando paginas dinamicamente

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,46 @@
+const { create } = require('domain');
+const path = require('path');
+
 module.exports.onCreateNode = ({ node, actions }) => {
   const { createNode, createNodeField } = actions;
 
   if (node.internal.type === 'MarkdownRemark') {
-
-    console.log(JSON.stringify(node, undefined, 4));
+    const slug = path.basename(node.fileAbsolutePath, '.md');
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    });
   }
+};
 
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const blogTemplate = path.resolve('./src/templates/blog.js');
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  res.data.allMarkdownRemark.edges.forEach((edge) => {
+    createPage({
+      component: blogTemplate,
+      path: `/blog/${edge.node.fields.slug}`,
+      context: {
+        slug: edge.node.fields.slug,
+      },
+    });
+  });
+  // 1. Get path
+  // 2. Get markdown data
+  // 3. Create new pages
 };
---
+++ src/templates/blog.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+
+function Blog() {
+  return (
+    <Layout>
+      This is a Blog Template
+    </Layout>
+  )
+}
+
+export default Blog
