677593ea608bc1dca735f43e6464bb143802e3d0
diego <diegodiko05@gmail.com>
2020-10-12 09:43:21 -0300

    Criando conteudo do blog via arquivos markDown

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,13 +1,28 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from 'react';
+import Layout from '../components/layout';
+import { graphql } from 'gatsby';
 
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`;
 
-function Blog() {
+function Blog(props) {
   return (
     <Layout>
-      This is a Blog Template
+        <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+        <p>{props.data.markdownRemark.frontmatter.date}</p>
+        {console.log(typeof props.data.markdownRemark.html)}
+        <div dangerouslySetInnerHTML={{__html: props.data.markdownRemark.html}} />
     </Layout>
-  )
+  );
 }
 
-export default Blog
+export default Blog;
