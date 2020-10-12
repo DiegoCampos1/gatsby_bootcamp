dfa0cb18be21361d68243155d6a425629916a90f
diego <diegodiko05@gmail.com>
2020-10-12 09:46:20 -0300

    Removendo console log e adicionando key no map

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -27,7 +27,7 @@ const BlogPage = () => {
       <ol>
         {data.allMarkdownRemark.edges.map((edge) => {
           return (
-            <li>
+            <li key={edge.node.fields.slug}>
               <Link to={`/blog/${edge.node.fields.slug}`}>
                 <h2>{edge.node.frontmatter.title}</h2>
                 <p>{edge.node.frontmatter.date}</p>
